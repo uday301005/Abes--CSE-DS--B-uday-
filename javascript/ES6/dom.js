6a1ce24c48c6b1ed60cc6460fced866dbb456d7a
uday301005 <sukuna9540641428@gmail.com>
2025-10-14 11:07:04 +0530

    actual dom

---
+++ javascript/ES6/dom.js
@@ -0,0 +1,7 @@
+const div = document.createElement("div");
+const h1 = document.createElement("h1");
+
+h1.innerText = "Hello Abes CSE-DS-B";
+
+div.appendChild(h1);
+document.getElementById("root").appendChild(div);
